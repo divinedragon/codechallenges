e2ebaa3510b6d92e196ce37516501e8ed0e29b34
thelostdragon <thelostdragon@d3188df1-1bba-406c-8712-004e600cc43b>
2015-09-09 11:36:43 +0000

    Added Game of Life problem solution.

---
+++ codechallenges/src/main/java/com/divinedragon/kidas/codechallenges/gameoflife/deepak/GameOfLife.java
@@ -0,0 +1,48 @@
+package com.divinedragon.kidas.codechallenges.gameoflife.deepak;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameOfLife {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader input = new BufferedReader(
+                new InputStreamReader(ClassLoader.getSystemResourceAsStream("gameoflife.txt")));
+
+        int totalPatterns = Integer.parseInt(input.readLine());
+
+        RuleEngine ruleEngine = new RuleEngine();
+
+        List<Grid> grids = new ArrayList<Grid>(totalPatterns);
+        
+        for (int i = 0; i < totalPatterns; i++) {
+            String gridSizeLine = input.readLine();
+            int gridRows = Integer.parseInt(gridSizeLine.split(" ")[0]);
+            int gridCols = Integer.parseInt(gridSizeLine.split(" ")[1]);
+
+            Grid grid = new Grid(gridRows, gridCols, ruleEngine);
+
+            for (int row = 0; row < gridRows; row++) {
+                String[] rowStates = input.readLine().split(" ");
+                for (int col = 0; col < gridCols; col++) {
+                    grid.setState(row, col, "X".contentEquals(rowStates[col]));
+                }
+            }
+
+            grids.add(grid);
+        }
+
+        
+        for (Grid grid : grids) {
+            System.out.println("Initial Generation");
+            grid.print();
+            System.out.println("Next Generation");
+            grid.nextGeneration().print();
+            System.out.println("---------------------------------------------------");
+        }
+    }
+}
