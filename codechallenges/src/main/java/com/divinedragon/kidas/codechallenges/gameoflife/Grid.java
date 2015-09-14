3dab0e35496779c92289ccc0482809772d9d2568
thelostdragon <thelostdragon@d3188df1-1bba-406c-8712-004e600cc43b>
2015-09-14 12:36:34 +0000

    Test cases for Game of Life.

--- codechallenges/src/main/java/com/divinedragon/kidas/codechallenges/gameoflife/Grid.java
+++ codechallenges/src/main/java/com/divinedragon/kidas/codechallenges/gameoflife/Grid.java
@@ -1,5 +1,7 @@
 package com.divinedragon.kidas.codechallenges.gameoflife;
 
+import org.springframework.util.Assert;
+
 public class Grid {
 
     private int rows;
@@ -11,6 +13,11 @@ public class Grid {
     private RuleEngine ruleEngine;
 
     public Grid(final int rows, final int cols, final RuleEngine ruleEngine) {
+
+        Assert.isTrue(rows > 0, "Rows of Grid cannot be zero");
+        Assert.isTrue(cols > 0, "Columns of Grid cannot be zero");
+        Assert.notNull(ruleEngine, "RuleEngine object cannot be null");
+
         this.rows = cols;
         this.cols = rows;
         this.ruleEngine = ruleEngine;
@@ -18,10 +25,16 @@ public class Grid {
     }
 
     public void setState(final int x, final int y, final boolean isAlive) {
+
+        checkXY(x, y);
+
         pattern[x][y] = (isAlive) ? 'x' : '-';
     }
 
     public boolean getState(final int x, final int y) {
+
+        checkXY(x, y);
+
         return (pattern[x][y] == 'x');
     }
 
@@ -35,20 +48,22 @@ public class Grid {
         }
     }
 
-    public int getAliveNeighbourCount(final int row, final int col) {
+    public int getAliveNeighbourCount(final int x, final int y) {
 
-        int startRow = (row != 0) ? (row - 1) : 0;
-        int endRow = (row != (rows - 1)) ? (row + 1) : row;
+        checkXY(x, y);
+
+        int startRow = (x != 0) ? (x - 1) : 0;
+        int endRow = (x != (rows - 1)) ? (x + 1) : x;
 
         int aliveNeighbourCount = 0;
 
         for ( ; startRow <= endRow; startRow++) {
 
-            int startCol = (col != 0) ? (col - 1) : 0;
-            int endCol = (col != (cols - 1)) ? (col + 1) : col;
+            int startCol = (y != 0) ? (y - 1) : 0;
+            int endCol = (y != (cols - 1)) ? (y + 1) : y;
 
             for ( ; startCol <= endCol; startCol++) {
-                if ((startRow != row || startCol != col)
+                if ((startRow != x || startCol != y)
                         && ('x' == pattern[startRow][startCol])) {
                     aliveNeighbourCount++;
                 }
@@ -72,10 +87,12 @@ public class Grid {
         return newGrid;
     }
 
-    private boolean getNextGenerationState(final int row, final int col) {
-        
-        int aliveNeighbourCount = getAliveNeighbourCount(row, col);
-        boolean isAliveInCurrentGeneration = (pattern[row][col] == 'x');
+    public boolean getNextGenerationState(final int x, final int y) {
+
+        checkXY(x, y);
+
+        int aliveNeighbourCount = getAliveNeighbourCount(x, y);
+        boolean isAliveInCurrentGeneration = (pattern[x][y] == 'x');
 
         return ruleEngine.isAliveInNextGeneration(
                 isAliveInCurrentGeneration, aliveNeighbourCount);
@@ -87,5 +104,14 @@ public class Grid {
 
     public int getCols() {
         return cols;
+    }
+
+    public RuleEngine getRuleEngine() {
+        return ruleEngine;
+    }
+
+    private void checkXY(final int x, final int y) {
+        Assert.isTrue((x >= 0 && x < rows), "Row number is out of grid specification");
+        Assert.isTrue((y >= 0 && y < cols), "Column number is out of grid specification");
     }
 }
---
+++ codechallenges/src/test/java/com/divinedragon/kidas/codechallenges/gameoflife/RuleEngineTest.java
@@ -0,0 +1,41 @@
+package com.divinedragon.kidas.codechallenges.gameoflife;
+
+import org.testng.annotations.Test;
+import org.springframework.util.Assert;
+import org.testng.annotations.BeforeTest;
+
+public class RuleEngineTest {
+
+    private RuleEngine fixture;
+
+    @BeforeTest
+    public void beforeTest() {
+        fixture = new RuleEngine();
+    }
+
+    @Test
+    public void isAliveInNextGenerationRule1() {
+        Assert.isTrue(fixture.isAliveInNextGeneration(true, -1) == false);
+        Assert.isTrue(fixture.isAliveInNextGeneration(true, 0)  == false);
+        Assert.isTrue(fixture.isAliveInNextGeneration(true, 1)  == false);
+    }
+
+    @Test
+    public void isAliveInNextGenerationRule2() {
+        Assert.isTrue(fixture.isAliveInNextGeneration(true, 2) == true);
+        Assert.isTrue(fixture.isAliveInNextGeneration(true, 3) == true);
+    }
+
+    @Test
+    public void isAliveInNextGenerationRule3() {
+        Assert.isTrue(fixture.isAliveInNextGeneration(true, 4) == false);
+        Assert.isTrue(fixture.isAliveInNextGeneration(true, 5) == false);
+    }
+
+    @Test
+    public void isAliveInNextGenerationRule4() {
+        Assert.isTrue(fixture.isAliveInNextGeneration(false, 2) == false);
+        Assert.isTrue(fixture.isAliveInNextGeneration(false, 3) == true);
+        Assert.isTrue(fixture.isAliveInNextGeneration(false, 4) == false);
+    }
+}
