aafda799480c5f2496db14bd89bd291a3ee899d1
Deepak Shakya <justdpk@gmail.com>
2016-03-24 17:50:57 +0530

    Unit test for LargestPallindromeProduct.java

---
+++ codechallenges/src/test/java/com/divinedragon/codechallenges/projecteuler/problem4/LargestPalindromeProductTest.java
@@ -0,0 +1,26 @@
+package com.divinedragon.codechallenges.projecteuler.problem4;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LargestPalindromeProductTest {
+
+    @DataProvider(name = "testcases")
+    public Object[][] testCases() {
+        return new Object[][] {
+            {new Integer(2), new Integer(9009)},
+            {new Integer(3), new Integer(906609)}
+        };
+    }
+
+    @Test(dataProvider = "testcases")
+    public void testGetLargestPallindromeProduct(
+            final Integer numberOfDigits,
+            final Integer expectedResult) {
+
+        assertTrue(expectedResult == LargestPallindromeProduct.getLargestPallindromeProduct(
+                numberOfDigits), "Largest Pallindrome calculated incorrectly");
+    }
+}
