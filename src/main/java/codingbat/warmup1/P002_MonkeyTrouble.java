3d44fb8fd0cac53c4dcc962908374427ce6b2540
Deepak Shakya <justdpk@gmail.com>
2017-02-15 22:45:21 +0100

    GH-3: Removed extra space between conditions

--- src/main/java/codingbat/warmup1/P002_MonkeyTrouble.java
+++ src/main/java/codingbat/warmup1/P002_MonkeyTrouble.java
@@ -3,7 +3,6 @@ package codingbat.warmup1;
 public class P002_MonkeyTrouble {
 
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        return (aSmile && bSmile)  || (!aSmile && !bSmile);
+        return (aSmile && bSmile) || (!aSmile && !bSmile);
     }
 }
-
