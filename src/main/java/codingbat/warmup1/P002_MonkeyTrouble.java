12b47d88085d91ec8d1db740b213031a598c6fa3
Deepak Shakya <justdpk@gmail.com>
2017-02-15 22:37:03 +0100

    GH-3: Added Java class for Monkey Trouble and its method implementation

---
+++ src/main/java/codingbat/warmup1/P002_MonkeyTrouble.java
@@ -0,0 +1,9 @@
+package codingbat.warmup1;
+
+public class P002_MonkeyTrouble {
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return (aSmile && bSmile)  || (!aSmile && !bSmile);
+    }
+}
+
