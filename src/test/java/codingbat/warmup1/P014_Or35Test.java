7c5ffe32e2326b4a95990988cbcd5e50a15eab06
Deepak Shakya <justdpk@gmail.com>
2017-02-22 22:42:31 +0100

    GH-16: Adds the code and Junit test for CodingBat-Warmup1->or35

---
+++ src/test/java/codingbat/warmup1/P014_Or35Test.java
@@ -0,0 +1,62 @@
+package codingbat.warmup1;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class P014_Or35Test {
+
+    private P014_Or35 fixture;
+
+    @Before
+    public void setup() {
+        fixture = new P014_Or35();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenMultipleOf3() {
+        boolean expected = true;
+
+        int number = 6;
+
+        boolean actual = fixture.or35(number);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenMultipleOf5() {
+        boolean expected = true;
+
+        int number = 10;
+
+        boolean actual = fixture.or35(number);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenMultipleOf3And5() {
+        boolean expected = true;
+
+        int number = 15;
+
+        boolean actual = fixture.or35(number);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenNoMultiple() {
+        boolean expected = false;
+
+        int number = 7;
+
+        boolean actual = fixture.or35(number);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+}
