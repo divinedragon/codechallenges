6ba7e1ff61991510a23baa8476cc0a40fb80bfbf
Deepak Shakya <justdpk@gmail.com>
2017-02-22 22:58:03 +0100

    GH-18: Adds code and Junit test for CodingBat-Warmup1->startHi

---
+++ src/main/java/codingbat/warmup1/P016_StartHi.java
@@ -0,0 +1,8 @@
+package codingbat.warmup1;
+
+public class P016_StartHi {
+
+    public boolean startHi(String str) {
+        return str.startsWith("hi");
+    }
+}
---
+++ src/test/java/codingbat/warmup1/P016_StartHiTest.java
@@ -0,0 +1,36 @@
+package codingbat.warmup1;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class P016_StartHiTest {
+
+    private P016_StartHi fixture;
+
+    @Before
+    public void setup() {
+        fixture = new P016_StartHi();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenStartsWithHi() {
+        String str = "hi there";
+        boolean expected = true;
+        boolean actual = fixture.startHi(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenDoesntStartsWithHi() {
+        String str = "hello";
+        boolean expected = false;
+        boolean actual = fixture.startHi(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+}
