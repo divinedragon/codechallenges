2b954b486c794474d33bc49139aa6569581f19e7
Deepak Shakya <justdpk@gmail.com>
2017-02-22 23:33:03 +0100

    GH-19: Added more testcases for icyHot

--- src/test/java/codingbat/warmup1/P017_IcyHotTest.java
+++ src/test/java/codingbat/warmup1/P017_IcyHotTest.java
@@ -53,8 +53,8 @@ public class P017_IcyHotTest {
     }
 
     @Test
-    public void shouldReturnFalseWhenFirstIsGreaterThanZeroAndSecondIsGreaterThan100() {
-        int temp1 = 1;
+    public void shouldReturnFalseWhenFirstIsEqualToZeroAndSecondIsGreaterThan100() {
+        int temp1 = 0;
         int temp2 = 101;
 
         boolean expected = false;
@@ -65,8 +65,56 @@ public class P017_IcyHotTest {
     }
 
     @Test
-    public void shouldReturnFalseWhenFirstIsEqualToZeroAndSecondIsGreaterThan100() {
+    public void shouldReturnFalseWhenFirstIsEqualToZeroAndSecondIsLessThan100() {
         int temp1 = 0;
+        int temp2 = 99;
+
+        boolean expected = false;
+
+        boolean actual = fixture.icyHot(temp1, temp2);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenFirstIsEqualToZeroAndSecondIsEqualTo100() {
+        int temp1 = 0;
+        int temp2 = 100;
+
+        boolean expected = false;
+
+        boolean actual = fixture.icyHot(temp1, temp2);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenFirstIsGreaterThanZeroAndSecondIsLessThan100() {
+        int temp1 = 0;
+        int temp2 = 99;
+
+        boolean expected = false;
+
+        boolean actual = fixture.icyHot(temp1, temp2);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenFirstIsGreaterThanZeroAndSecondIsEqualTo100() {
+        int temp1 = 0;
+        int temp2 = 100;
+
+        boolean expected = false;
+
+        boolean actual = fixture.icyHot(temp1, temp2);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenFirstIsGreaterThanZeroAndSecondIsGreaterThan100() {
+        int temp1 = 1;
         int temp2 = 101;
 
         boolean expected = false;
