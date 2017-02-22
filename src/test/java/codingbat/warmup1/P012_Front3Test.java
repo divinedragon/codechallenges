2c3ef192cf06d9a248e12337fbf33982368b9a73
Deepak Shakya <justdpk@gmail.com>
2017-02-22 20:52:42 +0100

    GH-14: Adds code for CodingBat-Warmup1-front3

---
+++ src/test/java/codingbat/warmup1/P012_Front3Test.java
@@ -0,0 +1,54 @@
+package codingbat.warmup1;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class P012_Front3Test {
+
+    private P012_Front3 fixture;
+
+    @Before
+    public void setup() {
+        fixture = new P012_Front3();
+    }
+
+    @Test
+    public void shouldReturnCorrectStringWhenStringLengthIsLessThan3() {
+        String expected = "aaa";
+        String str = "a";
+        String actual = fixture.front3(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnCorrectStringWhenStringLengthIsGreaterThan3() {
+        String expected = "ChoChoCho";
+        String str = "Chocolate";
+        String actual = fixture.front3(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnCorrectStringWhenStringLengthIsEqualTo3() {
+        String expected = "JavJavJav";
+        String str = "Jav";
+        String actual = fixture.front3(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnCorrectStringWhenStringIsBlank() {
+        String expected = "";
+        String str = "";
+        String actual = fixture.front3(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+}
