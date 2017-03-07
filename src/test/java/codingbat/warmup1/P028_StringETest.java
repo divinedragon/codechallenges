bf515caeab999b42c56a40eb7b6ef811caf8b358
Deepak Shakya <justdpk@gmail.com>
2017-03-07 23:12:59 +0100

    GH-30: Adds Junit tests for CodingBat-Warmup1->stringE

--- src/test/java/codingbat/warmup1/P028_StringETest.java
+++ src/test/java/codingbat/warmup1/P028_StringETest.java
@@ -1,5 +1,90 @@
 package codingbat.warmup1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P028_StringETest {
 
+    private P028_StringE fixture;
+
+    @Before
+    public void setup() {
+        fixture = new P028_StringE();
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStringIsEmpty() {
+
+        String str = "";
+
+        boolean expected = false;
+
+        boolean actual = fixture.stringE(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStringIsNotEmptyAndHasZeroE() {
+
+        String str = "a";
+
+        boolean expected = false;
+
+        boolean actual = fixture.stringE(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenStringIsNotEmptyAndHasOneE() {
+
+        String str = "ae";
+
+        boolean expected = true;
+
+        boolean actual = fixture.stringE(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenStringIsNotEmptyAndHasTwoE() {
+
+        String str = "aee";
+
+        boolean expected = true;
+
+        boolean actual = fixture.stringE(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenStringIsNotEmptyAndHasThreeE() {
+
+        String str = "aebece";
+
+        boolean expected = true;
+
+        boolean actual = fixture.stringE(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStringIsNotEmptyAndHasFourE() {
+
+        String str = "aebecede";
+
+        boolean expected = false;
+
+        boolean actual = fixture.stringE(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
