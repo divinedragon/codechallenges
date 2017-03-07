26d5cb2280159c63818d738c43ff3dc453987c38
Deepak Shakya <justdpk@gmail.com>
2017-03-07 23:40:15 +0100

    GH-31: Adds JUnit test case for CodingBat-Warmup1->lastDigit

--- src/test/java/codingbat/warmup1/P029_LastDigitTest.java
+++ src/test/java/codingbat/warmup1/P029_LastDigitTest.java
@@ -1,5 +1,42 @@
 package codingbat.warmup1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P029_LastDigitTest {
 
+    private P029_LastDigit fixture;
+
+    @Before
+    public void setup() {
+        fixture = new P029_LastDigit();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenLastDigitsAreSame() {
+        int num1 = 17;
+        int num2 = 7;
+
+        boolean expected = true;
+
+        boolean actual = fixture.lastDigit(num1, num2);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenLastDigitsAreNotSame() {
+        int num1 = 17;
+        int num2 = 13;
+
+        boolean expected = false;
+
+        boolean actual = fixture.lastDigit(num1, num2);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
