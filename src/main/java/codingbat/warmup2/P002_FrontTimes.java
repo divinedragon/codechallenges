666cec26785022139e1daf9f798320dc0eb3f512
Deepak Shakya <justdpk@gmail.com>
2017-03-16 22:15:31 +0100

    GH-65: Adds blank Java and JUnit tests for CodingBat-Warmup2->frontTimes

---
+++ src/main/java/codingbat/warmup2/P002_FrontTimes.java
@@ -0,0 +1,9 @@
+package codingbat.warmup2;
+
+public class P002_FrontTimes {
+
+    public String frontTimes(String str, int n) {
+        // TODO
+        return null;
+    }
+}
---
+++ src/test/java/codingbat/warmup2/P002_FrontTimesTest.java
@@ -0,0 +1,5 @@
+package codingbat.warmup2;
+
+public class P002_FrontTimesTest {
+
+}
