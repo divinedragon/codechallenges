6ae59ce90213ff3bb7c4010e24d743b1a7ff60a5
Deepak Shakya <justdpk@gmail.com>
2017-03-17 01:31:07 +0100

    GH-80: Adds blank Java and JUnit tests for CodingBat-Warmup2->has271

---
+++ src/main/java/codingbat/warmup2/P017_Has271.java
@@ -0,0 +1,9 @@
+package codingbat.warmup2;
+
+public class P017_Has271 {
+
+    public boolean has271(int[] nums) {
+        // TODO
+        return false;
+    }
+}
---
+++ src/test/java/codingbat/warmup2/P017_Has271Test.java
@@ -0,0 +1,5 @@
+package codingbat.warmup2;
+
+public class P017_Has271Test {
+
+}
