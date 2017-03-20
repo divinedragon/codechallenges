61168dcf831e25b4c8ac66decffec178b5f84a61
Deepak Shakya <justdpk@gmail.com>
2017-03-20 21:51:07 +0100

    GH-65: Java and Junit test cases for CodingBat-Warmup2->frontTimes

--- src/main/java/codingbat/warmup2/P002_FrontTimes.java
+++ src/main/java/codingbat/warmup2/P002_FrontTimes.java
@@ -3,7 +3,18 @@ package codingbat.warmup2;
 public class P002_FrontTimes {
 
     public String frontTimes(String str, int n) {
-        // TODO
-        return null;
+
+        StringBuilder result = new StringBuilder();
+
+        if (str != null && !str.isEmpty()) {
+
+            String front = (str.length() < 3) ? str : str.substring(0, 3);
+
+            for (int i = 1; i <= n; i++) {
+                result.append(front);
+            }
+        }
+
+        return result.toString();
     }
 }
