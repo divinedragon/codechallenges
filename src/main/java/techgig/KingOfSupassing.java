8b9783c824ad9ff089f7d863855e25f66294af7d
Deepak Shakya <justdpk@gmail.com>
2017-03-24 11:59:12 +0100

    GH-84: Adds blank Java and JUnit test file

---
+++ src/main/java/techgig/KingOfSupassing.java
@@ -0,0 +1,9 @@
+package techgig;
+
+public class KingOfSupassing {
+
+    public static int[] SurpassersKing(int[] input1) {
+        //Write code here
+        return null;
+    }
+}
---
+++ src/test/java/techgig/KingOfSupassingTest.java
@@ -0,0 +1,5 @@
+package techgig;
+
+public class KingOfSupassingTest {
+
+}
