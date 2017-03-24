d144664584efcad17d44ede29c272144e1a2b3a7
Deepak Shakya <justdpk@gmail.com>
2017-03-24 13:22:22 +0100

    GH-84: Code and Unit test for King of Supassing

--- src/main/java/techgig/KingOfSupassing.java
+++ src/main/java/techgig/KingOfSupassing.java
@@ -1,9 +1,25 @@
 package techgig;
 
+import java.util.Arrays;
+
 public class KingOfSupassing {
 
-    public static int[] SurpassersKing(int[] input1) {
-        //Write code here
-        return null;
+    public static int[] SurpassersKing(final int[] input1) {
+        int result[] = new int[input1.length];
+
+        int lastIndex = input1.length;
+
+        for (int i = 0; i < lastIndex - 1; i++) {
+            int count = 0;
+            for (int j = i + 1; j < lastIndex; j++) {
+                if (input1[i] < input1[j]) {
+                    count++;
+                }
+            }
+
+            result[i] = count;
+        }
+
+        return result;
     }
 }
--- src/test/java/techgig/KingOfSupassingTest.java
+++ src/test/java/techgig/KingOfSupassingTest.java
@@ -1,5 +1,30 @@
 package techgig;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class KingOfSupassingTest {
 
+    @Test
+    public void testcase1() {
+        int[] input = { 7, 2, 7, 5, 3, 0, 8, 1 };
+        int[] expected = { 1, 4, 1, 1, 1, 2, 0, 0 };
+
+        int[] actual = KingOfSupassing.SurpassersKing(input);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        int[] input = { 2, 7, 5, 5, 2, 7, 0, 8, 1 };
+        int[] expected = { 5, 1, 2, 2, 2, 1, 2, 0, 0 };
+
+        int[] actual = KingOfSupassing.SurpassersKing(input);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
