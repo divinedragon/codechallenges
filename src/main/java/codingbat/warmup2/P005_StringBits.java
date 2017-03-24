f2914bc07eae2b4b6c40ac7ff862130eef22eee8
Deepak Shakya <justdpk@gmail.com>
2017-03-24 19:52:47 +0100

    GH-68: Adds Java and Junit class for CodingBat-Warmup2-stringBits

--- src/main/java/codingbat/warmup2/P005_StringBits.java
+++ src/main/java/codingbat/warmup2/P005_StringBits.java
@@ -3,7 +3,17 @@ package codingbat.warmup2;
 public class P005_StringBits {
 
     public String stringBits(String str) {
-        // TODO
-        return null;
+
+        if (str == null || str.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i += 2) {
+            result.append(str.charAt(i));
+        }
+
+        return result.toString();
     }
 }
