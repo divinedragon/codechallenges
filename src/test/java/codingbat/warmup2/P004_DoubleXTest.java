de43d97ac966d36b43eb18890499199f071bedca
Deepak Shakya <justdpk@gmail.com>
2017-03-24 19:40:45 +0100

    GH-67: Adds Java and Junit class for CodingBat-Warmup2-doubleX

--- src/test/java/codingbat/warmup2/P004_DoubleXTest.java
+++ src/test/java/codingbat/warmup2/P004_DoubleXTest.java
@@ -1,5 +1,151 @@
 package codingbat.warmup2;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P004_DoubleXTest {
+
+    private P004_DoubleX fixture;
+
+    @Before
+    public void setup() {
+        fixture = new P004_DoubleX();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenFirstTwoXAreNextToEachOtherInFront() {
+
+        String str = "xxbb";
+
+        boolean expected = true;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenFirstTwoXAreNextToEachOtherInEnd() {
+
+        String str = "axx";
+
+        boolean expected = true;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenFirstTwoXAreNextToEachOtherInMiddle() {
+
+        String str = "axxbb";
+
+        boolean expected = true;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNoXExists() {
+
+        String str = "abcdef";
+
+        boolean expected = false;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenXExistsAlone() {
+
+        String str = "axcxefx";
+
+        boolean expected = false;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenFirstXIsAlone() {
+
+        String str = "xcxx";
+
+        boolean expected = false;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenAllX() {
+
+        String str = "xxx";
+
+        boolean expected = true;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenSingleX() {
+
+        String str = "x";
+
+        boolean expected = false;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTwoX() {
+
+        String str = "xx";
+
+        boolean expected = true;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNull() {
+
+        String str = null;
+
+        boolean expected = false;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenEmpty() {
+
+        String str = "";
+
+        boolean expected = false;
+
+        boolean actual = fixture.doubleX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 
 }
