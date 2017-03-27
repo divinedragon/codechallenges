34e504e561daf13eddc1cb79d9ab515ca25e23e9
Deepak Shakya <justdpk@gmail.com>
2017-03-27 23:58:11 +0200

    GH-74: Adds Java and Junit test classes for CodingBat-Warmup2-stringMatch

--- src/main/java/codingbat/warmup2/P011_StringMatch.java
+++ src/main/java/codingbat/warmup2/P011_StringMatch.java
@@ -3,7 +3,25 @@ package codingbat.warmup2;
 public class P011_StringMatch {
 
     public int stringMatch(String a, String b) {
-        // TODO
-        return 0;
+
+        if (a == null || a.isEmpty() || b == null || b.isEmpty()) {
+            return 0;
+        }
+
+        int loopCounter = Math.min(a.length(), b.length());
+
+        int totalMatches = 0;
+
+        for (int i = 0; i < loopCounter - 1; i++) {
+
+            String subStringA = a.substring(i, i + 2);
+            String subStringB = b.substring(i, i + 2);
+
+            if (subStringA.equals(subStringB)) {
+                totalMatches++;
+            }
+        }
+
+        return totalMatches;
     }
 }
