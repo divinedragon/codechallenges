46d08acd8123eecc2454393b08a92198c4cc1923
Deepak Shakya <justdpk@gmail.com>
2017-03-28 19:02:11 +0200

    GH-78: Adds Java and JUnit test for CodingBat-Warmup2-array667

--- src/main/java/codingbat/warmup2/P015_Array667.java
+++ src/main/java/codingbat/warmup2/P015_Array667.java
@@ -1,9 +1,32 @@
 package codingbat.warmup2;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class P015_Array667 {
 
-    public int array667(int[] nums) {
-        // TODO
-        return 0;
+    public int array667(final int[] nums) {
+
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+
+        StringBuilder numString = new StringBuilder();
+
+        Arrays.stream(nums).forEach(num -> numString.append(num));
+
+        Pattern pattern = Pattern.compile("6[67]");
+        Matcher matcher = pattern.matcher(numString);
+
+        int count = 0;
+        int startIndex = 0;
+
+        while (matcher.find(startIndex)) {
+            startIndex = matcher.end() - 1;
+            count++;
+        }
+
+        return count;
     }
 }
