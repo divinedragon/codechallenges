1e5defc47439add0ba8c96f803345020caa0ebcf
Deepak Shakya <justdpk@gmail.com>
2017-03-28 17:49:19 +0200

    GH-76: Adds Java and Junit test case for CodingBat-Warmup2-altPairs

--- src/main/java/codingbat/warmup2/P013_AltPairs.java
+++ src/main/java/codingbat/warmup2/P013_AltPairs.java
@@ -2,8 +2,58 @@ package codingbat.warmup2;
 
 public class P013_AltPairs {
 
-    public String altPairs(String str) {
-        // TODO
-        return null;
+    public String altPairs(final String str) {
+
+        if (str == null) {
+            return "";
+        }
+
+        if (str.isEmpty() || str.length() <= 2) {
+            return str;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        int count = 0;
+        int currentCharPos = 2 * count;
+        int nextCharPos = currentCharPos + 1;
+        int stringLength = str.length();
+
+        do {
+
+            result.append(str.charAt(currentCharPos));
+
+            if (nextCharPos < stringLength) {
+                result.append(str.charAt(nextCharPos));
+            }
+
+            count += 2;
+
+            currentCharPos = 2 * count;
+            nextCharPos = currentCharPos + 1;
+
+        } while (currentCharPos < stringLength);
+
+        return result.toString();
     }
+
+    /********************************************************
+     *    This is the official solution on the website.     *
+     ********************************************************
+    public String altPairsSolution(String str) {
+        String result = "";
+
+        // Run i by 4 to hit 0, 4, 8, ...
+        for (int i = 0; i < str.length(); i += 4) {
+            // Append the chars between i and i+2
+            int end = i + 2;
+            if (end > str.length()) {
+                end = str.length();
+            }
+            result = result + str.substring(i, end);
+        }
+
+        return result;
+    }
+    */
 }
--- src/test/java/codingbat/warmup2/P013_AltPairsTest.java
+++ src/test/java/codingbat/warmup2/P013_AltPairsTest.java
@@ -1,4 +1,94 @@
 package codingbat.warmup2;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P013_AltPairsTest {
+
+    private P013_AltPairs fixture;
+
+    @Before
+    public void setup() {
+        fixture = new P013_AltPairs();
+    }
+
+    @Test
+    public void testcase1() {
+
+        String str = "kitten";
+
+        String expected = "kien";
+
+        String actual = fixture.altPairs(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+
+        String str = "Chocolate";
+
+        String expected = "Chole";
+
+        String actual = fixture.altPairs(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+
+        String str = "CodingHorror";
+
+        String expected = "Congrr";
+
+        String actual = fixture.altPairs(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyStringWhenStringIsNull() {
+
+        String str = null;
+
+        String expected = "";
+
+        String actual = fixture.altPairs(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyStringWhenStringIsEmpty() {
+
+        String str = "";
+
+        String expected = "";
+
+        String actual = fixture.altPairs(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnSameStringWhenStringLengthIsLessThanEqual2() {
+
+        String str1 = "a";
+        String str2 = "ab";
+
+        String expected1 = "a";
+        String expected2 = "ab";
+
+        String actual1 = fixture.altPairs(str1);
+        String actual2 = fixture.altPairs(str2);
+
+        assertThat(actual1, is(equalTo(expected1)));
+        assertThat(actual2, is(equalTo(expected2)));
+    }
 }
