58a8de0b766afd2442903c9ca04a5532d9cf064a
Deepak Shakya <justdpk@gmail.com>
2017-03-28 18:06:53 +0200

    GH-77: Adds Java and JUnit test for CodingBat-Warmup2-stringYak

--- src/main/java/codingbat/warmup2/P014_StringYak.java
+++ src/main/java/codingbat/warmup2/P014_StringYak.java
@@ -2,8 +2,16 @@ package codingbat.warmup2;
 
 public class P014_StringYak {
 
-    public String stringYak(String str) {
-        // TODO
-        return null;
+    public String stringYak(final String str) {
+
+        if (str == null) {
+            return "";
+        }
+
+        if (str.isEmpty() || str.length() < 3) {
+            return str;
+        }
+
+        return str.replaceAll("y[a-zA-Z0-9]{1}k", "");
     }
 }
