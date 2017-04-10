388e7ba4172e78ca367a3ce7e31dab2794cf9271
Deepak Shakya <justdpk@gmail.com>
2017-04-10 22:59:18 +0200

    GH-115: Adds blank Java and Junit test classes

---
+++ src/main/java/codingbat/string1/P07_FirstHalf.java
@@ -0,0 +1,8 @@
+package codingbat.string1;
+
+public class P07_FirstHalf {
+
+    public String firstHalf(final String str) {
+        return null;
+    }
+}
---
+++ src/test/java/codingbat/string1/P07_FirstHalfTest.java
@@ -0,0 +1,5 @@
+package codingbat.string1;
+
+public class P07_FirstHalfTest {
+
+}
