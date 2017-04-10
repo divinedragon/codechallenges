5f6e2861a27b429116533e958072d781732f532d
Deepak Shakya <justdpk@gmail.com>
2017-04-10 23:24:18 +0200

    GH-119: Adds blank Java and Junit test classes

---
+++ src/main/java/codingbat/string1/P11_Left2.java
@@ -0,0 +1,8 @@
+package codingbat.string1;
+
+public class P11_Left2 {
+
+    public String left2(final String str) {
+        return null;
+    }
+}
---
+++ src/test/java/codingbat/string1/P11_Left2Test.java
@@ -0,0 +1,5 @@
+package codingbat.string1;
+
+public class P11_Left2Test {
+
+}
