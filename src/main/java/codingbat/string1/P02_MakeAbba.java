e19ac43b50b9ec550e15005194f8675ed7d6dba9
Deepak Shakya <justdpk@gmail.com>
2017-04-12 18:44:00 +0200

    GH-110: Adds Java code for CodingBat-String1-makeAbba

--- src/main/java/codingbat/string1/P02_MakeAbba.java
+++ src/main/java/codingbat/string1/P02_MakeAbba.java
@@ -3,6 +3,19 @@ package codingbat.string1;
 public class P02_MakeAbba {
 
     public String makeAbba(final String a, final String b) {
-        return null;
+
+        StringBuilder result = new StringBuilder();
+
+        String aNormalized = (a != null && !a.isEmpty()) ? a : "";
+        String bNormalized = (b != null && !b.isEmpty()) ? b : "";
+
+        // @formatter:off
+        return result
+                .append(aNormalized)
+                .append(bNormalized)
+                .append(bNormalized)
+                .append(aNormalized)
+                .toString();
+        // @formatter:on
     }
 }
--- src/test/java/codingbat/string1/P02_MakeAbbaTest.java
+++ src/test/java/codingbat/string1/P02_MakeAbbaTest.java
@@ -1,5 +1,148 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P02_MakeAbbaTest {
 
+    private P02_MakeAbba fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P02_MakeAbba();
+    }
+
+    @Test
+    public void shouldReturnEmptyStringWhenBothAreNull() {
+
+        String a = null;
+        String b = null;
+
+        String expected = "";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyStringWhenBothAreEmpty() {
+
+        String a = "";
+        String b = "";
+
+        String expected = "";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnBBWhenAIsNullAndBIsNotNull() {
+
+        String a = null;
+        String b = "B";
+
+        String expected = "BB";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnBBWhenAIsEmptyAndBIsNotNull() {
+
+        String a = "";
+        String b = "B";
+
+        String expected = "BB";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnAAWhenAIsNotNullAndBIsNull() {
+
+        String a = "A";
+        String b = null;
+
+        String expected = "AA";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnBBWhenAIsNotNullAndBIsNull() {
+
+        String a = "A";
+        String b = "";
+
+        String expected = "AA";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnBBWhenAIsNotNullAndBIsNotNullUpper() {
+
+        String a = "A";
+        String b = "B";
+
+        String expected = "ABBA";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnBBWhenAIsNotNullAndBIsNotNullLower() {
+
+        String a = "a";
+        String b = "b";
+
+        String expected = "abba";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnBBWhenAIsNotNullAndBIsNotNullMixed() {
+
+        String a = "A";
+        String b = "b";
+
+        String expected = "AbbA";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnBBWhenAIsNotNullAndBIsNotNullMultiChar() {
+
+        String a = "Abc";
+        String b = "Xyz";
+
+        String expected = "AbcXyzXyzAbc";
+
+        String actual = fixture.makeAbba(a, b);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
