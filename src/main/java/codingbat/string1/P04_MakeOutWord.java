12579f331365cb4ebf109748a669943ee54fc530
Deepak Shakya <justdpk@gmail.com>
2017-05-06 21:44:54 +0200

    GH-112: Adds Java and Junit classes for CodingBat-String1-makeOutWord

--- src/main/java/codingbat/string1/P04_MakeOutWord.java
+++ src/main/java/codingbat/string1/P04_MakeOutWord.java
@@ -3,6 +3,18 @@ package codingbat.string1;
 public class P04_MakeOutWord {
 
     public String makeOutWord(final String out, final String word) {
-        return null;
+
+        if (word == null || word.isEmpty()) {
+            return "";
+        }
+
+        String prefix = "";
+        String suffix = "";
+        if (out != null && out.length() > 0) {
+            prefix = out.substring(0, out.length() / 2);
+            suffix = out.substring(out.length() / 2);
+        }
+
+        return new StringBuilder().append(prefix).append(word).append(suffix).toString();
     }
 }
