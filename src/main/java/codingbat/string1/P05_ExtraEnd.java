f9a9c3ed0156d560184d55988aee8010daff23ec
Deepak Shakya <justdpk@gmail.com>
2017-05-06 22:05:00 +0200

    GH-113: Adds Java and Junit class for CodingBat-String1-extraEnd

--- src/main/java/codingbat/string1/P05_ExtraEnd.java
+++ src/main/java/codingbat/string1/P05_ExtraEnd.java
@@ -3,6 +3,18 @@ package codingbat.string1;
 public class P05_ExtraEnd {
 
     public String extraEnd(final String str) {
-        return null;
+
+        if (str == null || str.isEmpty()) {
+            return "";
+        }
+
+        String suffix = str.substring(Math.max(str.length() - 2, 0));
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 3; i > 0; i--) {
+            result.append(suffix);
+        }
+
+        return result.toString();
     }
 }
--- src/test/java/codingbat/string1/P05_ExtraEndTest.java
+++ src/test/java/codingbat/string1/P05_ExtraEndTest.java
@@ -1,5 +1,84 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P05_ExtraEndTest {
 
+    private P05_ExtraEnd fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P05_ExtraEnd();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "Hello";
+
+        String expected = "lololo";
+
+        String actual = fixture.extraEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "ab";
+
+        String expected = "ababab";
+
+        String actual = fixture.extraEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "Hi";
+
+        String expected = "HiHiHi";
+
+        String actual = fixture.extraEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsNull() {
+        String str = null;
+
+        String expected = "";
+
+        String actual = fixture.extraEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsEmpty() {
+        String str = "";
+
+        String expected = "";
+
+        String actual = fixture.extraEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnStringWhenStrLengthIsEqualTo1() {
+        String str = "a";
+
+        String expected = "aaa";
+
+        String actual = fixture.extraEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
