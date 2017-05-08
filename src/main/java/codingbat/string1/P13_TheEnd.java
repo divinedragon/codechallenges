5a278692491b0e3707b834f8ac2bba062e4518f2
Deepak Shakya <justdpk@gmail.com>
2017-05-09 00:33:03 +0200

    GH-121: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P13_TheEnd.java
+++ src/main/java/codingbat/string1/P13_TheEnd.java
@@ -1,8 +1,14 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P13_TheEnd {
 
     public String theEnd(final String str, final boolean front) {
-        return null;
+        String s = Optional.ofNullable(str).orElse("");
+
+        String frontChar = s.substring(0, Math.min(s.length(), 1));
+        String lastChar = s.substring(Math.max(0, s.length() - 1));
+        return (front) ? frontChar : lastChar;
     }
 }
