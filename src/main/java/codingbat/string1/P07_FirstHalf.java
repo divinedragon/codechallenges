32ad0aaffc55b1ccffddf7da728703c2bd0bf5e0
Deepak Shakya <justdpk@gmail.com>
2017-05-08 19:02:25 +0200

    GH-115: Adds Java and Junit test for CodingBat-String1-firstHalf

--- src/main/java/codingbat/string1/P07_FirstHalf.java
+++ src/main/java/codingbat/string1/P07_FirstHalf.java
@@ -3,6 +3,10 @@ package codingbat.string1;
 public class P07_FirstHalf {
 
     public String firstHalf(final String str) {
-        return null;
+        if (str == null || str.isEmpty()) {
+            return "";
+        }
+
+        return str.substring(0, (int) Math.ceil(str.length() / 2.0));
     }
 }
--- src/test/java/codingbat/string1/P07_FirstHalfTest.java
+++ src/test/java/codingbat/string1/P07_FirstHalfTest.java
@@ -1,5 +1,96 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P07_FirstHalfTest {
 
+    private P07_FirstHalf fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P07_FirstHalf();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "WooHoo";
+
+        String expected = "Woo";
+
+        String actual = fixture.firstHalf(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "HelloThere";
+
+        String expected = "Hello";
+
+        String actual = fixture.firstHalf(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "abcdef";
+
+        String expected = "abc";
+
+        String actual = fixture.firstHalf(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyStringWhenStrIsNull() {
+        String str = null;
+
+        String expected = "";
+
+        String actual = fixture.firstHalf(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyStringWhenStrIsEmpty() {
+        String str = "";
+
+        String expected = "";
+
+        String actual = fixture.firstHalf(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnSameStringWhenStrIsSingleChar() {
+        String str = "a";
+
+        String expected = "a";
+
+        String actual = fixture.firstHalf(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFirstCharWhenStrIsDoubleChar() {
+        String str = "ab";
+
+        String expected = "a";
+
+        String actual = fixture.firstHalf(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
