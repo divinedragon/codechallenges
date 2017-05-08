814aa3a80e8373d475e3f699e21c89ad2607bbf2
Deepak Shakya <justdpk@gmail.com>
2017-05-08 09:09:09 +0200

    GH-114: Adds Java and Junit test classes for CodingBat-String1-firstTwo

--- src/main/java/codingbat/string1/P06_FirstTwo.java
+++ src/main/java/codingbat/string1/P06_FirstTwo.java
@@ -3,6 +3,10 @@ package codingbat.string1;
 public class P06_FirstTwo {
 
     public String firstTwo(final String str) {
-        return null;
+        if (str == null || str.isEmpty()) {
+            return "";
+        }
+
+        return str.substring(0, Math.min(str.length(), 2));
     }
 }
