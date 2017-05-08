93f70620a74e55cb3f4cd4ebc576651d03aa1404
Deepak Shakya <justdpk@gmail.com>
2017-05-09 00:14:55 +0200

    GH-120: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P12_Right2.java
+++ src/main/java/codingbat/string1/P12_Right2.java
@@ -1,8 +1,16 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P12_Right2 {
 
     public String right2(final String str) {
-        return null;
+        String returnStr = Optional.ofNullable(str).orElse("");
+
+        if (returnStr.length() <= 2) {
+            return returnStr;
+        }
+
+        return returnStr.substring(returnStr.length() - 2) + returnStr.substring(0, returnStr.length() - 2);
     }
 }
--- src/test/java/codingbat/string1/P12_Right2Test.java
+++ src/test/java/codingbat/string1/P12_Right2Test.java
@@ -1,5 +1,95 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P12_Right2Test {
 
+    private P12_Right2 fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P12_Right2();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "Hi";
+
+        String expected = "Hi";
+
+        String actual = fixture.right2(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "java";
+
+        String expected = "vaja";
+
+        String actual = fixture.right2(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "Hello";
+
+        String expected = "loHel";
+
+        String actual = fixture.right2(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsNull() {
+        String str = null;
+
+        String expected = "";
+
+        String actual = fixture.right2(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsEmpty() {
+        String str = "";
+
+        String expected = "";
+
+        String actual = fixture.right2(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnSameStringWhenStrIsSingleChar() {
+        String str = "a";
+
+        String expected = "a";
+
+        String actual = fixture.right2(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnSameStringWhenStrIsDoubleChar() {
+        String str = "ab";
+
+        String expected = "ab";
+
+        String actual = fixture.right2(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
