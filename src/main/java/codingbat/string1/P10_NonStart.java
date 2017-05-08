e0c3781744461a2b0b52a0f19eead0711ae82cbd
Deepak Shakya <justdpk@gmail.com>
2017-05-08 21:32:33 +0200

    GH-118: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P10_NonStart.java
+++ src/main/java/codingbat/string1/P10_NonStart.java
@@ -1,8 +1,17 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P10_NonStart {
 
     public String nonStart(final String a, final String b) {
-        return null;
+
+        String strA = Optional.ofNullable(a).orElse("");
+        String strB = Optional.ofNullable(b).orElse("");
+
+        String A = strA.substring(Math.min(strA.length(), 1));
+        String B = strB.substring(Math.min(strB.length(), 1));
+
+        return A + B;
     }
 }
