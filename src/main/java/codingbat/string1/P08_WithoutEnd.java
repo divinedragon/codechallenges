b66880347843c853ebf42514d8694722ec3a6829
Deepak Shakya <justdpk@gmail.com>
2017-05-08 19:08:33 +0200

    GH-116: Adds Java and Junit test classes for CodingBat-String1-withoutEnd

--- src/main/java/codingbat/string1/P08_WithoutEnd.java
+++ src/main/java/codingbat/string1/P08_WithoutEnd.java
@@ -2,6 +2,11 @@ package codingbat.string1;
 
 public class P08_WithoutEnd {
     public String withoutEnd(final String str) {
-        return null;
+
+        if (str == null || str.isEmpty() || str.length() < 2) {
+            return "";
+        }
+
+        return str.substring(1, str.length() - 1);
     }
 }
--- src/test/java/codingbat/string1/P08_WithoutEndTest.java
+++ src/test/java/codingbat/string1/P08_WithoutEndTest.java
@@ -1,5 +1,95 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P08_WithoutEndTest {
 
+    private P08_WithoutEnd fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P08_WithoutEnd();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "Hello";
+
+        String expected = "ell";
+
+        String actual = fixture.withoutEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "java";
+
+        String expected = "av";
+
+        String actual = fixture.withoutEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "coding";
+
+        String expected = "odin";
+
+        String actual = fixture.withoutEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsNull() {
+        String str = null;
+
+        String expected = "";
+
+        String actual = fixture.withoutEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsEmpty() {
+        String str = "";
+
+        String expected = "";
+
+        String actual = fixture.withoutEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsSingleChar() {
+        String str = "a";
+
+        String expected = "";
+
+        String actual = fixture.withoutEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsDoubleChar() {
+        String str = "ab";
+
+        String expected = "";
+
+        String actual = fixture.withoutEnd(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
