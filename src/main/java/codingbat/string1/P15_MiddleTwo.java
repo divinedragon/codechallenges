d6509c432ff76fa85b44a8cf70403205967016e2
Deepak Shakya <justdpk@gmail.com>
2017-05-10 22:41:16 +0200

    GH-123: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P15_MiddleTwo.java
+++ src/main/java/codingbat/string1/P15_MiddleTwo.java
@@ -1,8 +1,24 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P15_MiddleTwo {
 
     public String middleTwo(final String str) {
-        return null;
+
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        if (strNormalized.length() > 0) {
+            if (strNormalized.length() % 2 != 0) {
+                throw new UnsupportedOperationException("MiddleTwo for odd lengths is not supported");
+            }
+
+            int middlePos = (strNormalized.length() / 2) - 1;
+
+            return strNormalized.substring(middlePos, middlePos + 2);
+        }
+
+        return "";
+
     }
 }
--- src/test/java/codingbat/string1/P15_MiddleTwoTest.java
+++ src/test/java/codingbat/string1/P15_MiddleTwoTest.java
@@ -1,5 +1,83 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P15_MiddleTwoTest {
+
+    private P15_MiddleTwo fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P15_MiddleTwo();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "string";
+
+        String expected = "ri";
+
+        String actual = fixture.middleTwo(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "code";
+
+        String expected = "od";
+
+        String actual = fixture.middleTwo(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "Practice";
+
+        String expected = "ct";
+
+        String actual = fixture.middleTwo(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsNull() {
+        String str = null;
+
+        String expected = "";
+
+        String actual = fixture.middleTwo(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsEmpty() {
+        String str = "";
+
+        String expected = "";
+
+        String actual = fixture.middleTwo(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldReturnEmptyWhenStrIsOddLength() {
+
+        fixture.middleTwo("a");
+        fail("Odd length string didn't throw exception");
+    }
 
 }
