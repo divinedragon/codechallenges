1d579144c8ac8fe02ba55bbfdacb67ba7774b817
Deepak Shakya <justdpk@gmail.com>
2017-05-10 22:51:04 +0200

    GH-124: Adds Java and Junit test cases

--- src/main/java/codingbat/string1/P16_EndsLy.java
+++ src/main/java/codingbat/string1/P16_EndsLy.java
@@ -1,8 +1,13 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P16_EndsLy {
 
     public boolean endsLy(final String str) {
-        return false;
+
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        return strNormalized.endsWith("ly");
     }
 }
--- src/test/java/codingbat/string1/P16_EndsLyTest.java
+++ src/test/java/codingbat/string1/P16_EndsLyTest.java
@@ -1,5 +1,73 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P16_EndsLyTest {
 
+    private P16_EndsLy fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P16_EndsLy();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "oddly";
+
+        boolean expected = true;
+
+        boolean actual = fixture.endsLy(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "y";
+
+        boolean expected = false;
+
+        boolean actual = fixture.endsLy(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "oddy";
+
+        boolean expected = false;
+
+        boolean actual = fixture.endsLy(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStrIsNull() {
+        String str = null;
+
+        boolean expected = false;
+
+        boolean actual = fixture.endsLy(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStrIsEmpty() {
+        String str = "";
+
+        boolean expected = false;
+
+        boolean actual = fixture.endsLy(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
