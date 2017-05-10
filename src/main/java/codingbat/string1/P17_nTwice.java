22a6107eb9695959267a87bcbb36b3b2941eed14
Deepak Shakya <justdpk@gmail.com>
2017-05-11 00:10:57 +0200

    GH-125: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P17_nTwice.java
+++ src/main/java/codingbat/string1/P17_nTwice.java
@@ -3,6 +3,18 @@ package codingbat.string1;
 public class P17_nTwice {
 
     public String nTwice(final String str, final int n) {
-        return null;
+
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+
+        if (str.length() < n) {
+            throw new IllegalArgumentException("Length of the String is too short");
+        }
+
+        String front = str.substring(0, n);
+        String end = str.substring(str.length() - n);
+
+        return front + end;
     }
 }
--- src/test/java/codingbat/string1/P17_nTwiceTest.java
+++ src/test/java/codingbat/string1/P17_nTwiceTest.java
@@ -1,5 +1,86 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P17_nTwiceTest {
 
+    private P17_nTwice fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P17_nTwice();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "Hello";
+        int n = 2;
+
+        String expected = "Helo";
+
+        String actual = fixture.nTwice(str, n);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "Chocolate";
+        int n = 3;
+
+        String expected = "Choate";
+
+        String actual = fixture.nTwice(str, n);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "Chocolate";
+        int n = 1;
+
+        String expected = "Ce";
+
+        String actual = fixture.nTwice(str, n);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsNull() {
+        String str = null;
+        int n = 1;
+
+        String expected = "";
+
+        String actual = fixture.nTwice(str, n);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsEmpty() {
+        String str = "";
+        int n = 1;
+
+        String expected = "";
+
+        String actual = fixture.nTwice(str, n);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenStrLengthIsSmallThanN() {
+        fixture.nTwice("abc", 5);
+
+        fail("Length smaller than N didn't throw exception");
+    }
 }
