fa70331bb07c7d25f9515b1c3da6d50dbaa3d93b
Deepak Shakya <justdpk@gmail.com>
2017-05-11 23:24:24 +0200

    GH-129: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P21_AtFirst.java
+++ src/main/java/codingbat/string1/P21_AtFirst.java
@@ -3,6 +3,15 @@ package codingbat.string1;
 public class P21_AtFirst {
 
     public String atFirst(final String str) {
-        return null;
+
+        if (str == null || str.isEmpty()) {
+            return "@@";
+        }
+
+        if (str.length() == 1) {
+            return str.substring(0, 1) + "@";
+        }
+
+        return str.substring(0, 2);
     }
 }
