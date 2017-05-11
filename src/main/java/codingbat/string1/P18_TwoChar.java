043b4fee761496fc468126a9458183f82fe12c24
Deepak Shakya <justdpk@gmail.com>
2017-05-11 23:35:18 +0200

    GH-126: Fixes for incorrect test cases

--- src/main/java/codingbat/string1/P18_TwoChar.java
+++ src/main/java/codingbat/string1/P18_TwoChar.java
@@ -4,11 +4,12 @@ public class P18_TwoChar {
 
     public String twoChar(final String str, final int index) {
 
-        if (str == null || str.length() < 2 || index < 0) {
+        if (str == null || str.length() < 2) {
             return "";
         }
 
-        int startIndex = ((index + 2) > str.length()) ? 0 : index;
+        int startIndex = (index > 0) ? index : 0;
+        startIndex = ((startIndex + 2) > str.length()) ? 0 : startIndex;
 
         return str.substring(startIndex, startIndex + 2);
     }
