219308627f4590daee20037712f5c3f43e0fef52
Deepak Shakya <justdpk@gmail.com>
2017-05-11 22:21:30 +0200

    GH-127: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P19_MiddleThree.java
+++ src/main/java/codingbat/string1/P19_MiddleThree.java
@@ -3,6 +3,13 @@ package codingbat.string1;
 public class P19_MiddleThree {
 
     public String middleThree(final String str) {
-        return null;
+
+        if (str == null || str.isEmpty() || str.length() % 2 == 0) {
+            return "";
+        }
+
+        int index = (str.length() - 3) / 2;
+
+        return str.substring(index, index + 3);
     }
 }
