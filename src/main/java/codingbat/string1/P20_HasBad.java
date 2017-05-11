57112dab111bc71ab039652b222c6c0b99a5cebf
Deepak Shakya <justdpk@gmail.com>
2017-05-11 22:40:51 +0200

    GH-128: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P20_HasBad.java
+++ src/main/java/codingbat/string1/P20_HasBad.java
@@ -1,8 +1,18 @@
 package codingbat.string1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class P20_HasBad {
 
     public boolean hasBad(final String str) {
-        return false;
+        if (str == null || str.isEmpty() || str.length() < 3) {
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile("^.{0,1}bad");
+        Matcher matcher = pattern.matcher(str);
+
+        return matcher.find();
     }
 }
--- src/test/java/codingbat/string1/P20_HasBadTest.java
+++ src/test/java/codingbat/string1/P20_HasBadTest.java
@@ -1,5 +1,107 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P20_HasBadTest {
 
+    private P20_HasBad fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P20_HasBad();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "badxx";
+
+        boolean expected = true;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "xbadxx";
+
+        boolean expected = true;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "xxbadxx";
+
+        boolean expected = false;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStrIsNull() {
+        String str = null;
+
+        boolean expected = false;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStrIsEmpty() {
+        String str = "";
+
+        boolean expected = false;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStrLengthIsLess3() {
+        String str = "ab";
+
+        boolean expected = false;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenStrLengthIsEqualTo3AndNotBad() {
+        String str = "abc";
+
+        boolean expected = false;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenStrLengthIsEqualTo3AndIsBad() {
+        String str = "bad";
+
+        boolean expected = true;
+
+        boolean actual = fixture.hasBad(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
