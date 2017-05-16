d183fcba6b720b8ad29126a6fc47e1f6f9e1ccf0
Deepak Shakya <justdpk@gmail.com>
2017-05-16 23:55:38 +0200

    GH-131: Adds Java and Junit classes for CodingBat-String1-conCat

--- src/main/java/codingbat/string1/P23_ConCat.java
+++ src/main/java/codingbat/string1/P23_ConCat.java
@@ -1,8 +1,20 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P23_ConCat {
 
     public String conCat(final String a, final String b) {
-        return null;
+        String aNormalized = Optional.ofNullable(a).orElse("");
+        String bNormalized = Optional.ofNullable(b).orElse("");
+
+        if (aNormalized.length() > 0 && bNormalized.length() > 0) {
+
+            if (aNormalized.charAt(aNormalized.length() - 1) == bNormalized.charAt(0)) {
+                return aNormalized.substring(0, aNormalized.length() - 1) + bNormalized;
+            }
+        }
+
+        return aNormalized + bNormalized;
     }
 }
