7bb8741da57b100adb8dca28c8813fad9a39e302
Deepak Shakya <justdpk@gmail.com>
2017-05-19 01:12:59 +0200

    GH-138: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P30_DeFront.java
+++ src/main/java/codingbat/string1/P30_DeFront.java
@@ -1,8 +1,17 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P30_DeFront {
 
-    public String makeAbba(final String a, final String b) {
-        return null;
+    public String deFront(final String str) {
+
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        String first = strNormalized.length() > 0 && 'a' == strNormalized.charAt(0) ? "a" : "";
+        String second = strNormalized.length() > 1 && 'b' == strNormalized.charAt(1) ? "b" : "";
+        String rest = strNormalized.length() > 2 ? strNormalized.substring(2) : "";
+
+        return first + second + rest;
     }
 }
