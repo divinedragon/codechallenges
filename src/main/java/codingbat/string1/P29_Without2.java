9eaa527d9853aca1d1b50b7e2d64edba8b067269
Deepak Shakya <justdpk@gmail.com>
2017-05-19 00:27:34 +0200

    GH-137: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P29_Without2.java
+++ src/main/java/codingbat/string1/P29_Without2.java
@@ -1,8 +1,21 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P29_Without2 {
 
     public String without2(final String str) {
-        return null;
+
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        if (strNormalized.length() == 0 || strNormalized.length() == 2) {
+            return "";
+        }
+
+        if (strNormalized.length() > 2
+                && strNormalized.substring(0, 2).equals(strNormalized.substring(strNormalized.length() - 2))) {
+            return strNormalized.substring(2);
+        }
+        return strNormalized;
     }
 }
