c46aead9e21831699d809887b476ae355eefd1ec
Deepak Shakya <justdpk@gmail.com>
2017-05-18 23:50:32 +0200

    GH-136: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P28_ExtraFront.java
+++ src/main/java/codingbat/string1/P28_ExtraFront.java
@@ -1,8 +1,16 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P28_ExtraFront {
 
     public String extraFront(final String str) {
-        return null;
+
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        int endIndex = Math.min(strNormalized.length(), 2);
+
+        return strNormalized.substring(0, endIndex) + strNormalized.substring(0, endIndex)
+                + strNormalized.substring(0, endIndex);
     }
 }
