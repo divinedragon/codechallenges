2089b26e3058c9e2a798351475e7ccc0698f49f4
Deepak Shakya <justdpk@gmail.com>
2017-05-18 21:43:09 +0200

    GH-133: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P25_SeeColor.java
+++ src/main/java/codingbat/string1/P25_SeeColor.java
@@ -1,8 +1,18 @@
 package codingbat.string1;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class P25_SeeColor {
 
     public String seeColor(final String str) {
-        return null;
+
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        Pattern pattern = Pattern.compile("^(red|blue)");
+        Matcher matcher = pattern.matcher(strNormalized);
+
+        return matcher.find() ? matcher.group(1) : "";
     }
 }
