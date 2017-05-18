24d241936216cfc867fb4b46e84f988aac846a55
Deepak Shakya <justdpk@gmail.com>
2017-05-18 22:20:29 +0200

    GH-134: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P26_FrontAgain.java
+++ src/main/java/codingbat/string1/P26_FrontAgain.java
@@ -1,8 +1,20 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P26_FrontAgain {
 
     public boolean frontAgain(final String str) {
-        return false;
+
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        if (strNormalized.length() >= 2) {
+            String firstTwo = strNormalized.substring(0, 2);
+            String lastTwo = strNormalized.substring(strNormalized.length() - 2);
+
+            return firstTwo.equals(lastTwo);
+        } else {
+            return false;
+        }
     }
 }
