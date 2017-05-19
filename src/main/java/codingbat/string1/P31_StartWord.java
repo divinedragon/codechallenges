d42d3027ae4d50d4c6f92bbae72f9ee03cea495b
Deepak Shakya <justdpk@gmail.com>
2017-05-20 01:34:41 +0200

    GH-139: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P31_StartWord.java
+++ src/main/java/codingbat/string1/P31_StartWord.java
@@ -1,8 +1,27 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P31_StartWord {
 
     public String startWord(final String str, final String word) {
-        return null;
+        String strNormalized = Optional.ofNullable(str).orElse("");
+        String wordNormalized = Optional.ofNullable(word).orElse("");
+
+        if (strNormalized.length() == 0 || wordNormalized.length() == 0) {
+            return "";
+        }
+
+        if (strNormalized.length() == 1 && wordNormalized.length() == 1) {
+            return strNormalized;
+        }
+
+        String search = wordNormalized.length() == 1 ? wordNormalized : wordNormalized.substring(1);
+        int startIndex = 1;
+        int endIndex = Math.min(strNormalized.length(), 1 + search.length());
+        String text = strNormalized.substring(startIndex, endIndex);
+
+        String matchedText = (wordNormalized.length() > 1) ? text : "";
+        return (text.equals(search)) ? strNormalized.charAt(0) + matchedText : "";
     }
 }
--- src/test/java/codingbat/string1/P31_StartWordTest.java
+++ src/test/java/codingbat/string1/P31_StartWordTest.java
@@ -1,5 +1,135 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P31_StartWordTest {
 
+    private P31_StartWord fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P31_StartWord();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "hippo";
+        String word = "hi";
+
+        String expected = "hi";
+
+        String actual = fixture.startWord(str, word);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "hippo";
+        String word = "xip";
+
+        String expected = "hip";
+
+        String actual = fixture.startWord(str, word);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "hippo";
+        String word = "i";
+
+        String expected = "h";
+
+        String actual = fixture.startWord(str, word);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenBothAreNull() {
+        String str = null;
+        String word = null;
+
+        String expected = "";
+
+        String actual = fixture.startWord(str, word);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenBothAreEmpty() {
+        String str = "";
+        String word = "";
+
+        String expected = "";
+
+        String actual = fixture.startWord(str, word);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenOneIsNullAndOtherIsEmpty() {
+        String str1 = null;
+        String word1 = "";
+        String str2 = "";
+        String word2 = null;
+
+        String expected = "";
+
+        String actual1 = fixture.startWord(str1, word1);
+        String actual2 = fixture.startWord(str2, word2);
+
+        assertThat(actual1, is(equalTo(expected)));
+        assertThat(actual2, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrLengthIsGreaterThanEqualTo1AndWordIsEmpty() {
+        String str1 = "h";
+        String word1 = "";
+        String str2 = "hh";
+        String word2 = "";
+
+        String expected = "";
+
+        String actual1 = fixture.startWord(str1, word1);
+        String actual2 = fixture.startWord(str2, word2);
+
+        assertThat(actual1, is(equalTo(expected)));
+        assertThat(actual2, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrLengthIsLessThanWordLength() {
+        String str = "h";
+        String word = "ix";
+
+        String expected = "";
+
+        String actual1 = fixture.startWord(str, word);
+
+        assertThat(actual1, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrLengthIsEqualToWordLength() {
+        String str = "h";
+        String word = "i";
+
+        String expected = "h";
+
+        String actual1 = fixture.startWord(str, word);
+
+        assertThat(actual1, is(equalTo(expected)));
+    }
 }
