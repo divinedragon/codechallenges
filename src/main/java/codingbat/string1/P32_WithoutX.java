4f4f0a258cfd1d44454f6fdc58fa75678aeabe93
Deepak Shakya <justdpk@gmail.com>
2017-05-20 01:55:00 +0200

    GH-140: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P32_WithoutX.java
+++ src/main/java/codingbat/string1/P32_WithoutX.java
@@ -1,8 +1,14 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P32_WithoutX {
 
     public String withoutX(final String str) {
-        return null;
+        return Optional
+                .ofNullable(str)
+                .orElse("")
+                .replaceAll("^x", "")
+                .replaceAll("x$", "");
     }
 }
--- src/test/java/codingbat/string1/P32_WithoutXTest.java
+++ src/test/java/codingbat/string1/P32_WithoutXTest.java
@@ -1,5 +1,128 @@
 package codingbat.string1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P32_WithoutXTest {
 
+    private P32_WithoutX fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P32_WithoutX();
+    }
+
+    @Test
+    public void testcase1() {
+        String str = "xHix";
+
+        String expected = "Hi";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        String str = "xHi";
+
+        String expected = "Hi";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        String str = "Hxix";
+
+        String expected = "Hxi";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsNull() {
+        String str = null;
+
+        String expected = "";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsEmpty() {
+        String str = "";
+
+        String expected = "";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsSingleCharNotX() {
+        String str = "a";
+
+        String expected = "a";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsSingleCharX() {
+        String str = "x";
+
+        String expected = "";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsMultiCharLeftX() {
+        String str = "xxAbxcD";
+
+        String expected = "xAbxcD";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsMultiCharRightX() {
+        String str = "AbxcDxx";
+
+        String expected = "AbxcDx";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenStrIsMultiCharLeftRightX() {
+        String str = "xxAbxcDxx";
+
+        String expected = "xAbxcDx";
+
+        String actual = fixture.withoutX(str);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
