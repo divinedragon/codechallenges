785e7f364a08ac80fbe5efcd5b56c397d064bfd2
Deepak Shakya <justdpk@gmail.com>
2017-05-20 02:20:22 +0200

    GH-141: Adds Java and Junit test classes

--- src/main/java/codingbat/string1/P33_WithoutX2.java
+++ src/main/java/codingbat/string1/P33_WithoutX2.java
@@ -1,8 +1,22 @@
 package codingbat.string1;
 
+import java.util.Optional;
+
 public class P33_WithoutX2 {
 
     public String withoutX2(final String str) {
-        return null;
+        String strNormalized = Optional.ofNullable(str).orElse("");
+
+        String front = "";
+        String tail = "";
+        if (strNormalized.length() <= 2) {
+            front = strNormalized;
+            tail = "";
+        } else {
+            front = strNormalized.substring(0, 2);
+            tail = strNormalized.substring(2);
+        }
+
+        return front.replaceAll("x", "") + tail;
     }
 }
