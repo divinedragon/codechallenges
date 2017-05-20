1036d632e86b64f363173aae7de02b0b8a662fc1
Deepak Shakya <justdpk@gmail.com>
2017-05-20 10:42:11 +0200

    GH-139: Removed codacy violations

--- src/main/java/codingbat/string1/P31_StartWord.java
+++ src/main/java/codingbat/string1/P31_StartWord.java
@@ -5,23 +5,24 @@ import java.util.Optional;
 public class P31_StartWord {
 
     public String startWord(final String str, final String word) {
-        String strNormalized = Optional.ofNullable(str).orElse("");
-        String wordNormalized = Optional.ofNullable(word).orElse("");
+        final String strNormalized = Optional.ofNullable(str).orElse("");
+        final String wordNormalized = Optional.ofNullable(word).orElse("");
 
         if (strNormalized.length() == 0 || wordNormalized.length() == 0) {
             return "";
-        }
-
-        if (strNormalized.length() == 1 && wordNormalized.length() == 1) {
+        } else  if (strNormalized.length() == 1 && wordNormalized.length() == 1) {
             return strNormalized;
+        } else if (strNormalized.length() == 1) {
+            return "";
         }
+        
+        
+        final String search = wordNormalized.substring(1);
+        final int startIndex = 1;
+        final int endIndex = Math.min(strNormalized.length(), 1 + search.length());
+        final String text = strNormalized.substring(startIndex, endIndex);
 
-        String search = wordNormalized.length() == 1 ? wordNormalized : wordNormalized.substring(1);
-        int startIndex = 1;
-        int endIndex = Math.min(strNormalized.length(), 1 + search.length());
-        String text = strNormalized.substring(startIndex, endIndex);
-
-        String matchedText = (wordNormalized.length() > 1) ? text : "";
-        return (text.equals(search)) ? strNormalized.charAt(0) + matchedText : "";
+        final String matchedText = wordNormalized.length() > 1 ? text : ""; // NOPMD by dragon on 20/5/17 2:29 AM
+        return text.equals(search) ? strNormalized.charAt(0) + matchedText : "";
     }
 }
