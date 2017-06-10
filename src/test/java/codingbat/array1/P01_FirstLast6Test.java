6f022e00ebaf6c84ce4827aff916324cb7fbf7fe
Deepak Shakya <justdpk@gmail.com>
2017-06-10 11:56:53 +0200

    GH-178: Adds Java and Junit class files for CodingBat-Array1-firstLast6

---
+++ src/test/java/codingbat/array1/P01_FirstLast6Test.java
@@ -0,0 +1,95 @@
+package codingbat.array1;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class P01_FirstLast6Test {
+
+    private P01_FirstLast6 fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P01_FirstLast6();
+    }
+
+    @Test
+    public void testcase1() {
+        int[] nums = new int[] { 1, 2, 6 };
+
+        boolean expected = true;
+
+        boolean actual = fixture.firstLast6(nums);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase2() {
+        int[] nums = new int[] { 6, 1, 2, 3 };
+
+        boolean expected = true;
+
+        boolean actual = fixture.firstLast6(nums);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void testcase3() {
+        int[] nums = new int[] { 13, 6, 1, 2, 3 };
+
+        boolean expected = false;
+
+        boolean actual = fixture.firstLast6(nums);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNumIsNull() {
+        int[] nums = null;
+
+        boolean expected = false;
+
+        boolean actual = fixture.firstLast6(nums);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNumIsEmpty() {
+        int[] nums = null;
+
+        boolean expected = false;
+
+        boolean actual = fixture.firstLast6(nums);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNumHasSingleElement6() {
+        int[] nums = new int[] { 6 };
+
+        boolean expected = true;
+
+        boolean actual = fixture.firstLast6(nums);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNumHasSingleElementNon6() {
+        int[] nums = new int[] { 1 };
+
+        boolean expected = false;
+
+        boolean actual = fixture.firstLast6(nums);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+}
