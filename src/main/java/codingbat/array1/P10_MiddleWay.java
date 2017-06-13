88edae7917241f14f8082388a62f460669a680ad
Deepak Shakya <justdpk@gmail.com>
2017-06-13 13:27:06 +0200

    GH-187: Fixes the codacy violations and simplifies the code

--- src/main/java/codingbat/array1/P10_MiddleWay.java
+++ src/main/java/codingbat/array1/P10_MiddleWay.java
@@ -6,37 +6,23 @@ public class P10_MiddleWay {
 
     public int[] middleWay(final int[] a, final int[] b) {
 
-        int[] aNormalized = Optional.ofNullable(a).orElse(new int[] {});
-        int[] bNormalized = Optional.ofNullable(b).orElse(new int[] {});
+        final int[] aNormalized = Optional.ofNullable(a).orElse(new int[] {});
+        final int[] bNormalized = Optional.ofNullable(b).orElse(new int[] {});
 
         int aMid = Integer.MIN_VALUE;
         int bMid = Integer.MIN_VALUE;
 
-        if ((aNormalized.length > 0 && aNormalized.length % 2 == 0)
-                || (bNormalized.length > 0 && bNormalized.length % 2 == 0)) {
+        if (aNormalized.length > 0 && aNormalized.length % 2 == 0) {
             throw new IllegalArgumentException();
         }
 
-        if (aNormalized.length > 0 && aNormalized.length % 2 != 0) {
-            aMid = aNormalized[aNormalized.length / 2];
+        if (bNormalized.length > 0 && bNormalized.length % 2 == 0) {
+            throw new IllegalArgumentException();
         }
 
-        if (bNormalized.length > 0 && bNormalized.length % 2 != 0) {
-            bMid = bNormalized[bNormalized.length / 2];
-        }
+        aMid = aNormalized[aNormalized.length / 2];
+        bMid = bNormalized[bNormalized.length / 2];
 
-        if (aMid > 0 && bMid > 0) {
-            return new int[] { aMid, bMid };
-        }
-
-        if (aMid > 0) {
-            return new int[] { aMid };
-        }
-
-        if (bMid > 0) {
-            return new int[] { bMid };
-        }
-
-        return new int[] {};
+        return new int[] { aMid, bMid };
     }
 }
