c13e27d2fce3bff019d788bd8ae5d95156dd5bcc
Deepak Shakya <justdpk@gmail.com>
2017-06-19 21:33:29 +0200

    GH-202: Adds Java and Junit class for CodingBat-Array1-unlucky1

--- src/main/java/codingbat/array1/P25_Unlucky1.java
+++ src/main/java/codingbat/array1/P25_Unlucky1.java
@@ -3,7 +3,6 @@ package codingbat.array1;
 import java.util.Optional;
 import java.util.function.Predicate;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -38,6 +37,29 @@ public class P25_Unlucky1 {
         return false;
     }
 
+    public boolean unlucky1NonPredicate(final int[] nums) {
+
+        final int[] numsNormalized = Optional.ofNullable(nums).orElse(new int[] {});
+
+        if (numsNormalized.length < 2) {
+            return false;
+        }
+
+        int i = 0;
+        int j = i + 1;
+        while (i < numsNormalized.length - 1) {
+
+            if ((i < 2 || i >= numsNormalized.length - 2) && (numsNormalized[i] == 1 && numsNormalized[j] == 3)) {
+                return true;
+            }
+
+            i++;
+            j++;
+        }
+
+        return false;
+    }
+
     @Getter
     @Setter
     private static final class Values {
@@ -54,6 +76,6 @@ public class P25_Unlucky1 {
     private Predicate<Values> isUnlucky1Predicate() {
 
         return p -> (p.getCurrentPosition() < 2 || p.getCurrentPosition() >= p.getArrayLength() - 2)
-                    && (p.getCurrentValue() == 1 && p.getNextValue() == 3);
+                && (p.getCurrentValue() == 1 && p.getNextValue() == 3);
     }
 }
