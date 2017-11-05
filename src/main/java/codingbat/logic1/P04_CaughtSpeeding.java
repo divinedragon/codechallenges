e3f8bad7cad61fb3cdeab2de545042eb4dfd540b
Deepak Shakya <justdpk@gmail.com>
2017-11-05 12:52:06 +0100

    GH-240: CodingBat-Logic1-caughtSpeeding

--- src/main/java/codingbat/logic1/P04_CaughtSpeeding.java
+++ src/main/java/codingbat/logic1/P04_CaughtSpeeding.java
@@ -2,7 +2,17 @@ package codingbat.logic1;
 
 public class P04_CaughtSpeeding {
 
-    public int caughtSpeeding(int speed, boolean isBirthday) {
-        return 0;
+    public int caughtSpeeding(final int speed, final boolean isBirthday) {
+
+        final int lowerLimit = (isBirthday) ? 66 : 61;
+        final int upperLimit = (isBirthday) ? 85 : 80;
+
+        if (speed < lowerLimit) {
+            return 0;
+        } else if (speed <= upperLimit) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 }
