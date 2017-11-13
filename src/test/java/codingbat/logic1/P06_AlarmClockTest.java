00b7b363e948835badd3090348a114e718c80b21
Deepak Shakya <justdpk@gmail.com>
2017-11-13 21:01:56 +0100

    GH-242: Java and Junit test class for CodingBat-Logic1-alarmClock

--- src/test/java/codingbat/logic1/P06_AlarmClockTest.java
+++ src/test/java/codingbat/logic1/P06_AlarmClockTest.java
@@ -1,5 +1,70 @@
 package codingbat.logic1;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P06_AlarmClockTest {
 
+    private P06_AlarmClock fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P06_AlarmClock();
+    }
+
+    @Test
+    public void shouldReturnOffWhenVacationAndWeekend() {
+
+        final int day = 5;
+        final boolean vacation = true;
+
+        final String expected = P06_AlarmClock.ALARM_OFF;
+
+        final String actual = fixture.alarmClock(day, vacation);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnOffWhenVacationAndWeekday() {
+
+        final int day = 2;
+        final boolean vacation = true;
+
+        final String expected = P06_AlarmClock.ALARM_10AM;
+
+        final String actual = fixture.alarmClock(day, vacation);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnOffWhenNotVacationAndWeekend() {
+
+        final int day = 6;
+        final boolean vacation = false;
+
+        final String expected = P06_AlarmClock.ALARM_10AM;
+
+        final String actual = fixture.alarmClock(day, vacation);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnOffWhenNotVacationAndWeekday() {
+
+        final int day = 4;
+        final boolean vacation = false;
+
+        final String expected = P06_AlarmClock.ALARM_7AM;
+
+        final String actual = fixture.alarmClock(day, vacation);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
 }
