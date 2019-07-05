9a34041cf2afbc85a60a11a4bba9488d641d19a6
Deepak Shakya <justdpk@gmail.com>
2019-07-05 23:44:10 +0200

    GH-245: Code for CodingBat-Logic1-specialEleven

--- src/test/java/codingbat/logic1/P09_SpecialElevenTest.java
+++ src/test/java/codingbat/logic1/P09_SpecialElevenTest.java
@@ -1,5 +1,53 @@
 package codingbat.logic1;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P09_SpecialElevenTest {
 
+    private P09_SpecialEleven fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P09_SpecialEleven();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenNumberIsAMultipleOf11() {
+
+        final int n = 22;
+
+        final boolean expected = true;
+
+        final boolean actual = fixture.specialEleven(n);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenNumberIs1MoreThanMultipleOf11() {
+
+        final int n = 23;
+
+        final boolean expected = true;
+
+        final boolean actual = fixture.specialEleven(n);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenNumberIsNotASpecialNumber() {
+
+        final int n = 24;
+
+        final boolean expected = false;
+
+        final boolean actual = fixture.specialEleven(n);
+
+        assertThat(actual, is(expected));
+    }
 }
