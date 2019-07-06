25b79b318c592a1470eee8eba4d408ed2c409662
Deepak Shakya <justdpk@gmail.com>
2019-07-06 18:16:47 +0200

    GH-247: CodingBat Logic1 old35

--- src/main/java/codingbat/logic1/P11_Odd35.java
+++ src/main/java/codingbat/logic1/P11_Odd35.java
@@ -2,7 +2,11 @@ package codingbat.logic1;
 
 public class P11_Odd35 {
 
-    public boolean old35(int n) {
-        return false;
+    public boolean old35(final int n) {
+
+        final boolean isDivisibleBy3 = (n % 3 == 0);
+        final boolean isDivisibleBy5 = (n % 5 == 0);
+
+        return (isDivisibleBy3 && !isDivisibleBy5) || (!isDivisibleBy3 && isDivisibleBy5);
     }
 }
--- src/test/java/codingbat/logic1/P11_Odd35Test.java
+++ src/test/java/codingbat/logic1/P11_Odd35Test.java
@@ -1,5 +1,65 @@
 package codingbat.logic1;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P11_Odd35Test {
 
+    private P11_Odd35 fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P11_Odd35();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenNIsDivisibleBy3ButNotDivisibleBy5() {
+
+        final int n = 3;
+
+        final boolean expected = true;
+
+        final boolean actual = fixture.old35(n);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenNIsNotDivisibleBy3ButDivisibleBy5() {
+
+        final int n = 5;
+
+        final boolean expected = true;
+
+        final boolean actual = fixture.old35(n);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNIsDivisibleBy3AndDivisibleBy5() {
+
+        final int n = 15;
+
+        final boolean expected = false;
+
+        final boolean actual = fixture.old35(n);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNIsNotDivisibleBy3AndNotDivisibleBy5() {
+
+        final int n = 7;
+
+        final boolean expected = false;
+
+        final boolean actual = fixture.old35(n);
+
+        assertThat(actual, is(expected));
+    }
 }
