14550b1d938e7ab7f0753a2054294aa3d470a946
Deepak Shakya <justdpk@gmail.com>
2019-07-11 23:45:59 +0200

    GH-252: Code for CodingBat-Logic1-teaParty

--- src/main/java/codingbat/logic1/P16_TeaParty.java
+++ src/main/java/codingbat/logic1/P16_TeaParty.java
@@ -2,7 +2,26 @@ package codingbat.logic1;
 
 public class P16_TeaParty {
 
-    public int teaParty(int tea, int candy) {
-        return 0;
+    public int teaParty(final int tea, final int candy) {
+
+        if (isGood(tea, candy) && isGreat(tea, candy)) {
+            return 2;
+        } else if (isGood(tea, candy)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private boolean isGreat(final int tea, final int candy) {
+
+        final int twiceTea = 2 * tea;
+        final int twiceCandy = 2 * candy;
+
+        return (tea >= twiceCandy) || (candy >= twiceTea);
+    }
+
+    private boolean isGood(final int tea, final int candy) {
+        return (tea >= 5 && candy >= 5);
     }
 }
--- src/test/java/codingbat/logic1/P16_TeaPartyTest.java
+++ src/test/java/codingbat/logic1/P16_TeaPartyTest.java
@@ -1,5 +1,68 @@
 package codingbat.logic1;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P16_TeaPartyTest {
 
+    private P16_TeaParty fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P16_TeaParty();
+    }
+
+    @Test
+    public void teaPartyShouldReturnTwoWhenPartyIsGreat() {
+
+        final int tea1 = 20;
+        final int tea2 = 6;
+        final int candy1 = 6;
+        final int candy2 = 20;
+
+        final int expected = 2;
+
+        final int actual1 = fixture.teaParty(tea1, candy1);
+        final int actual2 = fixture.teaParty(tea2, candy2);
+
+        assertThat(actual1, is(expected));
+        assertThat(actual2, is(expected));
+    }
+
+    @Test
+    public void teaPartyShouldReturnOneWhenPartyIsGood() {
+
+        final int tea1 = 6;
+        final int tea2 = 8;
+        final int candy1 = 8;
+        final int candy2 = 6;
+
+        final int expected = 1;
+
+        final int actual1 = fixture.teaParty(tea1, candy1);
+        final int actual2 = fixture.teaParty(tea2, candy2);
+
+        assertThat(actual1, is(expected));
+        assertThat(actual2, is(expected));
+    }
+
+    @Test
+    public void teaPartyShouldReturnZeroWhenPartyIsBad() {
+
+        final int tea1 = 3;
+        final int tea2 = 8;
+        final int candy1 = 8;
+        final int candy2 = 3;
+
+        final int expected = 0;
+
+        final int actual1 = fixture.teaParty(tea1, candy1);
+        final int actual2 = fixture.teaParty(tea2, candy2);
+
+        assertThat(actual1, is(expected));
+        assertThat(actual2, is(expected));
+    }
 }
