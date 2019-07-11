d31e361655fe3d13747482a8c7c2cac649255b71
Deepak Shakya <justdpk@gmail.com>
2019-07-12 00:06:33 +0200

    GH-253: CodingBat-Logic1-fizzBuzzString code

--- src/main/java/codingbat/logic1/P17_FizzString.java
+++ src/main/java/codingbat/logic1/P17_FizzString.java
@@ -1,8 +1,31 @@
 package codingbat.logic1;
 
+import java.util.Optional;
+
 public class P17_FizzString {
 
-    public String fizzString(String str) {
-        return null;
+    public String fizzString(final String str) {
+
+        final String string = Optional.ofNullable(str).orElse("").trim();
+
+        if (string.length() == 0) {
+            return string;
+        }
+
+        final int startPosition = 0;
+        final int lastPosition = string.length() - 1;
+
+        final String firstChar = String.valueOf(string.charAt(startPosition));
+        final String lastChar = String.valueOf(string.charAt(lastPosition));
+
+        if (firstChar.equalsIgnoreCase("f") && lastChar.equalsIgnoreCase("b")) {
+            return "FizzBuzz";
+        } else if (firstChar.equalsIgnoreCase("f")) {
+            return "Fizz";
+        } else if (lastChar.equalsIgnoreCase("b")) {
+            return "Buzz";
+        } else {
+            return string;
+        }
     }
 }
