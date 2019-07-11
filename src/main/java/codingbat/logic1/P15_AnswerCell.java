617d72cc7ae75e8b38f2dcdd2dcbce22b3aede7d
Deepak Shakya <justdpk@gmail.com>
2019-07-11 23:26:44 +0200

    GH-251: CodingBat-Logic1-answerCell

--- src/main/java/codingbat/logic1/P15_AnswerCell.java
+++ src/main/java/codingbat/logic1/P15_AnswerCell.java
@@ -2,7 +2,14 @@ package codingbat.logic1;
 
 public class P15_AnswerCell {
 
-    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
-        return false;
+    public boolean answerCell(final boolean isMorning, final boolean isMom, final boolean isAsleep) {
+
+        if (isAsleep) {
+            return false;
+        } else if (isMorning && !isMom) {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/test/java/codingbat/logic1/P15_AnswerCellTest.java
+++ src/test/java/codingbat/logic1/P15_AnswerCellTest.java
@@ -1,5 +1,87 @@
 package codingbat.logic1;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class P15_AnswerCellTest {
 
+    private P15_AnswerCell fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new P15_AnswerCell();
+    }
+
+    @Test
+    public void answerCellShouldReturnFalseWhenSleeping() {
+
+        final boolean isAsleep = true;
+        final boolean isMorning = false;
+        final boolean isMom = false;
+
+        final boolean expected = false;
+
+        final boolean actual = fixture.answerCell(isMorning, isMom, isAsleep);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void answerCellShouldReturnFalseWhenAwakeAndIsMorningAndNotMom() {
+
+        final boolean isAsleep = false;
+        final boolean isMorning = true;
+        final boolean isMom = false;
+
+        final boolean expected = false;
+
+        final boolean actual = fixture.answerCell(isMorning, isMom, isAsleep);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void answerCellShouldReturnTrueWhenAwakeAndIsMorningAndIsMom() {
+
+        final boolean isAsleep = false;
+        final boolean isMorning = true;
+        final boolean isMom = true;
+
+        final boolean expected = true;
+
+        final boolean actual = fixture.answerCell(isMorning, isMom, isAsleep);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void answerCellShouldReturnTrueWhenAwakeAndNotMorningAndIsMom() {
+
+        final boolean isAsleep = false;
+        final boolean isMorning = false;
+        final boolean isMom = true;
+
+        final boolean expected = true;
+
+        final boolean actual = fixture.answerCell(isMorning, isMom, isAsleep);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void answerCellShouldReturnTrueWhenAwakeAndNotMorningAndNotMom() {
+
+        final boolean isAsleep = false;
+        final boolean isMorning = false;
+        final boolean isMom = false;
+
+        final boolean expected = true;
+
+        final boolean actual = fixture.answerCell(isMorning, isMom, isAsleep);
+
+        assertThat(actual, is(expected));
+    }
 }
